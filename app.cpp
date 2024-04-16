f296ea877a6e0b80fdea0bd932ff7e95be1343b6
丁襄龍 <dingxianglong@dingxianglongdeMacBook-Pro.local>
2024-04-16 23:53:22 +0800

    initial commit msg

---
+++ app.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    char c[] ="test/0";
+
+    int a=5;
+    cout<<"aa "<< c <<endl;
+} 
---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"test"<<endl;
+    string name;
+
+    cin>>name;
+
+    cout<<"hellow"<<name<<ends;
+    cin.get();
+    // cin.get();
+    
+
+} 
